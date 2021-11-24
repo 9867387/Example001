df71e70e67ea0a2f7d9a9dbdb25f5a8b31c3ffaa
9867387 <tservice48@yandex.ru>
2021-11-24 14:08:33 +0300

    Hi_masha

---
+++ Example_005_Person_Hi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Как вас завут? : ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура этоже машаа");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
