93b069881d411d2724cf501fa08cf87e2ff8b607
9867387 <tservice48@yandex.ru>
2021-11-25 00:05:18 +0300

    гири

---
+++ Example_006Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 8;
+int b = 12;
+int c = 3;
+int d = 9;
+int e = 4;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
